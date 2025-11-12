02b70d8b0d192e303409a8938b54de5fc937ba1d
Vega Darsi <vegadarsiwork@gmail.com>
2025-11-12 15:22:29 +0530

    Fix TypeScript build error in dock component

--- components/ui/dock.tsx
+++ components/ui/dock.tsx
@@ -160,7 +160,7 @@ function DockItem({ children, className }: DockItemProps) {
       aria-haspopup='true'
     >
       {Children.map(children, (child) =>
-        cloneElement(child as React.ReactElement, { width, isHovered })
+        cloneElement(child as React.ReactElement, { width, isHovered } as any)
       )}
     </motion.div>
   );
